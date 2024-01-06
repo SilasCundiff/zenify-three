016e7d5bc702fc58718f3e2ebfedb785d011dd23
SilasCundiff <silinski23@gmail.com>
2024-01-06 14:04:38 -0500

    add an extra hide ui button

--- src/components/layoutComponents/topbar/Topbar.tsx
+++ src/components/layoutComponents/topbar/Topbar.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { faSignOut } from '@fortawesome/free-solid-svg-icons'
+import { faEye, faSignOut } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Leva } from 'leva'
 import { signOut } from 'next-auth/react'
@@ -7,7 +7,7 @@ import SearchInput from './SearchInput'
 import { useUI } from '@/helpers/hooks/useUI'
 
 function Topbar() {
-  const { uiHidden } = useUI()
+  const { uiHidden, setUiHidden } = useUI()
   return (
     <div className='relative flex w-full p-2 md:grid md:grid-cols-[240px_1fr_240px] md:p-4'>
       <div
@@ -20,17 +20,25 @@ function Topbar() {
       <div
         className={`${
           uiHidden ? 'pointer-events-none opacity-0' : 'opacity-100'
-        } mr-4 flex w-full transition-opacity duration-500  md:col-start-2 `}
+        } mr-4 flex w-full transition-opacity duration-500 md:col-start-2 `}
       >
         <SearchInput />
       </div>
-      <div
-        className={`${
-          uiHidden ? 'pointer-events-none opacity-0' : 'opacity-100'
-        } ml-2 transition-opacity duration-500 md:col-start-3 md:ml-auto`}
-      >
+      <div className={`ml-2 flex transition-opacity duration-500 md:col-start-3 md:ml-auto`}>
         <button
-          className='rounded-custom flex min-h-10 w-fit min-w-10 items-center bg-slate-50 p-2 font-semibold text-black md:px-4'
+          className={`${
+            uiHidden ? 'opacity-10' : 'opacity-100'
+          } rounded-custom pointer-events-auto flex min-h-10 w-fit min-w-10 items-center justify-center bg-slate-50 p-2 font-semibold text-black transition-opacity duration-500 hover:opacity-100 md:px-4`}
+          onClick={() => {
+            setUiHidden(!uiHidden)
+          }}
+        >
+          <FontAwesomeIcon icon={faEye} />
+        </button>
+        <button
+          className={`${
+            uiHidden ? 'pointer-events-none opacity-0' : 'opacity-100'
+          } rounded-custom ml-2 flex min-h-10 w-fit min-w-10 items-center bg-slate-50 p-2 font-semibold text-black transition-opacity duration-500 md:px-4 `}
           onClick={() => signOut()}
         >
           <span className='hidden md:inline'>Logout</span>
